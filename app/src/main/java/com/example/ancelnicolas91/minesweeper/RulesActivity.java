b940b05154458971ccb720b688939fb90a923dc2
nikomptepas <ancelnicolas@icloud.com>
2015-01-11 15:23:31 +0100

    Ajout des règles en FR et EN

---
+++ app/src/main/java/com/example/ancelnicolas91/minesweeper/RulesActivity.java
@@ -0,0 +1,17 @@
+package com.example.ancelnicolas91.minesweeper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class RulesActivity extends android.app.Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+
+
+
+}
